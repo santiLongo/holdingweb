7c1595ce5f5933840829c21a39efef8c29528c6e
esmor <esmor@192.168.0.22>
2024-11-24 12:16:39 -0300

    Nueva clase para instanciar los atributos del empresa

---
+++ src/main/java/prog2/holdingweb/EmpresaDTO.java
@@ -0,0 +1,89 @@
+package prog2.holdingweb;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+
+public class EmpresaDTO{
+    
+    private int codigo;
+    private String nombre;
+    private int facturacion;
+    private int cantVendedores;
+    private LocalDate fechaDeEntrada;
+    private ArrayList<AreaDTO> areas;
+    private ArrayList<PaisDTO> paises;
+    private PaisDTO sedeCentral;
+
+    public EmpresaDTO(){
+        this.areas = new ArrayList<>();
+        this.paises = new ArrayList<>();
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getFacturacion() {
+        return facturacion;
+    }
+
+    public void setFacturacion(int facturacion) {
+        this.facturacion = facturacion;
+    }
+
+    public int getCantVendedores() {
+        return cantVendedores;
+    }
+
+    public void setCantVendedores(int cantVendedores) {
+        this.cantVendedores = cantVendedores;
+    }
+
+    public LocalDate getFechaDeEntrada() {
+        return fechaDeEntrada;
+    }
+
+    public void setFechaDeEntrada(String fechaDeEntrada) {
+        this.fechaDeEntrada = LocalDate.parse(fechaDeEntrada);
+    }
+
+    public ArrayList<AreaDTO> getAreas() {
+        return areas;
+    }
+
+    public void setAreas(ArrayList<AreaDTO> areas) {
+        this.areas = areas;
+    }
+
+    public ArrayList<PaisDTO> getPaises() {
+        return paises;
+    }
+
+    public void setPaises(ArrayList<PaisDTO> paises) {
+        this.paises = paises;
+    }
+
+    public PaisDTO getSedeCentral() {
+        return sedeCentral;
+    }
+
+    public void setSedeCentral(PaisDTO sedeCentral) {
+        this.sedeCentral = sedeCentral;
+    }
+    
+    
+    
+}
