b2d02a553596c194a361f15aaa4cb0ac046a37d6
esmor <esmor@192.168.0.22>
2024-11-24 12:16:25 -0300

    Nueva clase para instanciar los atributos del asesor

--- src/main/java/prog2/holdingweb/AsesorDTO.java
+++ src/main/java/prog2/holdingweb/AsesorDTO.java
@@ -1,20 +1,81 @@
 package prog2.holdingweb;
 
 import java.time.LocalDate;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.ArrayList;
+import org.springframework.stereotype.Repository;
 
 public class AsesorDTO extends UsuarioDTO{
     
     private int codigo;
     private String nombre;
     private String direccion;
+    private ArrayList<AreaDTO> areas;
+    private ArrayList<Asesora> asesora;
     
-    public AsesorDTO(int cod, String us, String pas, String nom, String dir){
-        codigo = cod;
-        setUs(us);
-        setContraseña(pas);
-        nombre = nom;
-        direccion = dir;
+    public AsesorDTO(){
+        this.areas = new ArrayList<>();
+        this.asesora = new ArrayList<>();
     }
+    
+    public static class Asesora{
+        
+        private EmpresaDTO empresa;
+        private LocalDate fechaEntrada;
+        
+        public Asesora(EmpresaDTO empresa, String fechaEntrada){
+            this.empresa = empresa;
+            this.fechaEntrada = LocalDate.parse(fechaEntrada);
+        }
+
+        public EmpresaDTO getEmpresa() {
+            return empresa;
+        }
+
+        public LocalDate getFechaEntrada() {
+            return fechaEntrada;
+        }
+        
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public ArrayList<Asesora> getAsesora() {
+        return asesora;
+    }
+
+    public void setAsesora(ArrayList<Asesora> asesora) {
+        this.asesora = asesora;
+    }
+
+    public ArrayList<AreaDTO> getAreas() {
+        return areas;
+    }
+
+    public void setAreas(ArrayList<AreaDTO> areas) {
+        this.areas = areas;
+    }
+    
+    
 }
