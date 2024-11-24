1dc3c89d6d91ecd04389219d4edb46b68351ef5f
esmor <esmor@192.168.0.22>
2024-11-24 12:19:59 -0300

    Nueva clase para manejo de la base de datos cuando se trate de empresas

---
+++ src/main/java/prog2/holdingweb/EmpresaDAO.java
@@ -0,0 +1,62 @@
+
+package prog2.holdingweb;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public class EmpresaDAO {
+    
+    private final String dbFullURL; 
+    private final String dbUser; 
+    private final String dbPswd;
+    @Autowired
+    private PaisDAO paisDAO;
+    @Autowired
+    private VendedorDAO vendedorDAO;
+    @Autowired
+    private AreaDAO areaDAO;
+    
+    @Autowired 
+    public EmpresaDAO( 
+            @Qualifier("dbName") String dbName, 
+            @Qualifier("dbURL")  String dbURL, 
+            @Qualifier("dbUser") String dbUser, 
+            @Qualifier("dbPswd") String dbPswd) { 
+        dbFullURL = "jdbc:mysql://" + dbURL + "/" + dbName; 
+        this.dbUser = dbUser; 
+        this.dbPswd = dbPswd; 
+    }
+    
+    public EmpresaDTO cargarEmpresa(int codigo){
+        EmpresaDTO empresa = new EmpresaDTO();
+        empresa.setCodigo(codigo);
+        try {
+            Connection con = DriverManager.getConnection(dbFullURL, dbUser, dbPswd);
+            Statement stmt = con.createStatement(); 
+            stmt.execute("SELECT * FROM empresa WHERE id = " + codigo + ""); 
+            ResultSet rs = stmt.getResultSet(); 
+            rs.next();
+            empresa.setNombre(rs.getString(2));
+            empresa.setFacturacion(rs.getInt(3));
+            empresa.setCantVendedores(vendedorDAO.cantidadVendedores(codigo));
+            empresa.setFechaDeEntrada(rs.getString(5));
+            empresa.setSedeCentral(paisDAO.cargarPais(rs.getInt(6)));
+            empresa.setPaises(paisDAO.cargarListaPaises(codigo));
+            empresa.setAreas(areaDAO.cargarAreasCubre(codigo));
+        } catch (SQLException ex) {
+            Logger.getLogger(UsuarioDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return empresa;
+    }
+    
+}
